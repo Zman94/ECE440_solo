3ea2eeb4211716135cf075745317ce660c405d3c
Zman94 <zgleason94@gmail.com>
2016-10-14 18:52:50 -0500

    Finished wordoku

--- hw2/wordoku.py
+++ hw2/wordoku.py
@@ -10,41 +10,39 @@ def main():
         print("No word bank file or no puzzle. Provide a word bank and a puzzle to solve and rerun.")
         return
 
+    nodes_expanded = [0]
     w, h = 9, 9
     puzzle = [[0 for y in range(h)] for x in range(w)] #declare a 2d array to hold the puzzle
     word_bank = []
     frontierTree = {}
+    word_location = []
 
     readBank(sys.argv[1], word_bank) #word_bank now holds all the words given
     readPuzzle(sys.argv[2], puzzle, w)
     # word_bank=["AHGGGGGGG"]
 
-    hasAnswer = dfsExpandState(puzzle,word_bank,frontierTree)
+    hasAnswer = dfsExpandState(word_location, puzzle,word_bank,frontierTree, nodes_expanded)
 
     if not hasAnswer:
         print "The maze given has no solution for the given word bank."
         return
 
-
-    for layer in frontierTree.values():
-        for state in frontierTree[word_bank[-1]]:
-            for row in state:
-                for val in row:
-                    print '{:1}'.format(val),
-                print
+    ### Print execution information ###
+    print str(nodes_expanded[0])+" nodes were expanded."
+    print
+    for word in word_location:
+        print word[1][2]+","+word[1][1]+","+word[1][0]+": "+word[0]
+    print
+    for state in frontierTree[word_bank[-1]]:
+        if len(state) < 4:
+            continue
+        for row in state:
+            for val in row:
+                print '{:1}'.format(val),
             print
-            print
+        break
+    print
 
-    # while len(frontier)!=0:
-    #     expandState(puzzle, curWord, frontier)
-
-    # print frontier
-    # for row in puzzle:
-    #     for val in row:
-    #         print '{:1}'.format(val),
-    #     print
-    # word_bank.sort(key=len, reverse=True) #sort word_bank by most constraining to least constraining
-    # print word_bank
     return
 
 def readBank(input_file, word_bank):
@@ -65,23 +63,20 @@ def readPuzzle(input_file, puzzle, w):
             x+=1
             y=0
 
-def dfsExpandState(puzzle, word_bank, frontierTree, layer=0, direction=True):
+def dfsExpandState(word_location, puzzle, word_bank, frontierTree, nodes_expanded, layer=0, direction=True):
     if layer==len(word_bank):
         return True
 
-    ### Debugging Print Statements ###
-    # print word_bank[layer]
-    # for row in puzzle:
-    #     for val in row:
-    #         print '{:1}'.format(val),
-    #     print
-    # print
-
+    nodes_expanded[0]+=1
     expandState(puzzle, word_bank[layer], frontierTree, direction) #expand the first word into the state
 
     for frontier in frontierTree[word_bank[layer]]:
-        hasAnswer = dfsExpandState(frontier, word_bank, frontierTree, layer+1, not direction)
+        if len(frontier) < 4: #skip any states that contain the coordinates of the words assignment and update the last used coordinates
+            coordinates=frontier
+            continue
+        hasAnswer = dfsExpandState(word_location, frontier, word_bank, frontierTree, nodes_expanded, layer+1, not direction)
         if hasAnswer:
+            word_location.insert(0,[word_bank[layer],coordinates])
             return True
     return False
 
@@ -121,6 +116,7 @@ def expandState(puzzle, curWord, frontier, direction):
                         #         print '{:1}'.format(val),
                         #     print
 
+                        frontier[curWord].append(str(x)+str(y)+"V")
                         frontier[curWord].append(puzzleCpy)
                         break
                     if y+z >=9:
@@ -145,6 +141,7 @@ def expandState(puzzle, curWord, frontier, direction):
                         #         print '{:1}'.format(val),
                         #     print
 
+                        frontier[curWord].append(str(x)+str(y)+"H")
                         frontier[curWord].append(puzzleCpy)
                         break
                     if x+z >=9:
@@ -169,6 +166,7 @@ def expandState(puzzle, curWord, frontier, direction):
                         #         print '{:1}'.format(val),
                         #     print
 
+                        frontier[curWord].append(str(x)+str(y)+"H")
                         frontier[curWord].append(puzzleCpy)
                         break
                     if x+z >=9:
@@ -193,6 +191,7 @@ def expandState(puzzle, curWord, frontier, direction):
                         #         print '{:1}'.format(val),
                         #     print
 
+                        frontier[curWord].append(str(x)+str(y)+"V")
                         frontier[curWord].append(puzzleCpy)
                         break
                     if y+z >=9:
