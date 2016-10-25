c75bc5afb73490221fcf102fca7818021788b331
Zman94 <zgleason94@gmail.com>
2016-10-24 20:18:29 -0500

    pushing for a temp file

--- hw2/test.py
+++ hw2/test.py
@@ -8,9 +8,9 @@ heuristic.append(15) # Center
 print "Aggressive Heuristic:",heuristic
 
 heuristic = list()
-heuristic.append(20) # Your pieces
-heuristic.append(-40)# Opponent's pieces
-heuristic.append(30) # Position
-heuristic.append(10) # Opponent's position
+heuristic.append(40) # Your pieces
+heuristic.append(-20)# Opponent's pieces
+heuristic.append(10) # Position
+heuristic.append(30) # Opponent's position
 heuristic.append(15) # Center
 print "Defensive Heuristic:",heuristic
