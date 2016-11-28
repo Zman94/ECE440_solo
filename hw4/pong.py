e8b5d1fb494e8026a7b2e5dd5d608f7043ebd9f4
Zman94 <zgleason94@gmail.com>
2016-11-28 11:18:09 -0800

    Learning algorithm works

--- hw4/pong.py
+++ hw4/pong.py
@@ -2,12 +2,11 @@ from __future__ import print_function
 import pygame, sys, time
 import pdb
 from pygame.locals import *
-from random import randint
+import random
 
 start_time = time.time()
 
 # Number of frames per second
-# Change this value to speed up or slow down the game
 FPS = 60
 
 #Global Variables to be used through the program
@@ -29,9 +28,12 @@ columns = 12
 victory_reward = 1
 failure_reward = -1
 gamma = .8
-alpha = 1
-learningRate = 1
+alpha = 1.0
+learning_times = [[[[[0 for v in range(3)] for w in range(2)] for x in range(columns)] for y in range(columns)] for z in range(rows)]
+Nc = 1
 
+#File to store trained list
+out_file = "./q_training.txt"
 
 #Draws the arena the game will be played in. 
 def drawArena():
@@ -70,32 +72,34 @@ def checkEdgeCollision(ball, velocity_x, velocity_y):
 #Checks is the ball has hit a paddle, and 'bounces' ball off it.
 def checkHitBall(ball, paddle1, paddle2, velocity_x, velocity_y):
     if velocity_x < 0 and paddle1.right >= ball.left and paddle1.top < ball.bottom and paddle1.bottom > ball.top:
+        return -velocity_x, velocity_y
+    # if velocity_x < 0 and paddle1.right >= ball.left and paddle1.top < ball.bottom and paddle1.bottom > ball.top:
+    #     velocity_x*=-1
+    #     speed_up=(random.randint(-150,150)/10000.0)
+    #     velocity_x+=speed_up
+    #     velocity_y+=(random.randint(-300,300)/10000.0)
+    #     if velocity_x < .03:
+    #         velocity_x = .03
+    #     # if velocity_x > .1:
+    #     #     velocity_x = .1
+    #     # if velocity_y > .09:
+    #     #     velocity_y = .09
+    #     if velocity_y < -.06:
+    #         velocity_y = -.06
+    #     return velocity_x, velocity_y
+    if velocity_x > 0 and paddle2.left <= ball.right and paddle2.top < ball.bottom and paddle2.bottom > ball.top:
         velocity_x*=-1
-        speed_up=(randint(-150,150)/10000.0)
+        speed_up=(random.randint(-150,150)/10000.0)
         velocity_x+=speed_up
-        velocity_y+=(randint(-300,300)/10000.0)
-        if velocity_x < .03:
-            velocity_x = .03
-        if velocity_x > .1:
-            velocity_x = .1
-        if velocity_y > .09:
-            velocity_y = .09
-        if velocity_y < -.06:
-            velocity_y = -.06
-        return velocity_x, velocity_y
-    elif velocity_x > 0 and paddle2.left <= ball.right and paddle2.top < ball.bottom and paddle2.bottom > ball.top:
-        velocity_x*=-1
-        speed_up=(randint(-150,150)/10000.0)
-        velocity_x+=speed_up
-        velocity_y+=(randint(-300,300)/10000.0)
+        velocity_y+=(random.randint(-300,300)/10000.0)
         if velocity_x > -.03:
             velocity_x = -.03
-        if velocity_x < -.1:
-            velocity_x = -.1
-        if velocity_y > .06:
-            velocity_y = .06
-        if velocity_y < -.06:
-            velocity_y = -.06
+        # if velocity_x < -.1:
+        #     velocity_x = -.1
+        # if velocity_y > .06:
+        #     velocity_y = .06
+        # if velocity_y < -.06:
+        #     velocity_y = -.06
         return velocity_x, velocity_y
     else:
         return velocity_x, velocity_y
@@ -127,36 +131,35 @@ def hard_coded_paddle(ball, paddle1):
 
 #Q trained computer player
 def q_paddle(q, cur_state, paddle2):
-    probability = randint(1,101)
+    print(cur_state)
+    curminus1 = cur_state[2]-1
+    curplus1 = cur_state[2]+1
 
-    if q[cur_state[0],cur_state[1],cur_state[2]-1,cur_state[3],cur_state[4]] > q[cur_state[0],cur_state[1],cur_state[2]+1,cur_state[3],cur_state[4]]:
-        if q[cur_state[0],cur_state[1],cur_state[2]-1,cur_state[3],cur_state[4]] > q[cur_state[0],cur_state[1],cur_state[2],cur_state[3],cur_state[4]]:
-            max_state = 1
-        else:
-            max_state = 0
-    else:
-        if q[cur_state[0],cur_state[1],cur_state[2]+1,cur_state[3],cur_state[4]] > q[cur_state[0],cur_state[1],cur_state[2],cur_state[3],cur_state[4]]:
-            max_state = -1
-        else:
-            max_state = 0
+    if cur_state[2]-1 < 0:
+        curminus1 = 0
+    if cur_state[2]+1 >= rows:
+        curplus1 = rows-1
+    print(learning_times[cur_state[0]][cur_state[1]][curminus1][cur_state[3]][cur_state[4]])
+    print(learning_times[cur_state[0]][cur_state[1]][cur_state[2]][cur_state[3]][cur_state[4]])
+    print(learning_times[cur_state[0]][cur_state[1]][curplus1][cur_state[3]][cur_state[4]])
 
-    if probability > 80:
-        best_move = False
-    else:
-        best_move = True
-
-    #Add randomness to the move (may remove later)
-    if not best_move:
-        if probability > 90:
-            max_state+=2
-        else:
-            max_state+=1
-
-    #Ensure valid max state number
-    if max_state == 3:
-        max_state = 0
-    if max_state == 2:
+    if learning_times[cur_state[0]][cur_state[1]][curminus1][cur_state[3]][cur_state[4]] < Nc:
         max_state = -1
+    elif learning_times[cur_state[0]][cur_state[1]][cur_state[2]][cur_state[3]][cur_state[4]] < Nc:
+        max_state = 0
+    elif learning_times[cur_state[0]][cur_state[1]][curplus1][cur_state[3]][cur_state[4]] < Nc:
+        max_state = 1
+    else: #only enter the utility function if all explorations are satisfied
+        if q[cur_state[0]][cur_state[1]][curminus1][cur_state[3]][cur_state[4]] > q[cur_state[0]][cur_state[1]][curplus1][cur_state[3]][cur_state[4]]:
+            if q[cur_state[0]][cur_state[1]][curminus1][cur_state[3]][cur_state[4]] > q[cur_state[0]][cur_state[1]][cur_state[2]][cur_state[3]][cur_state[4]]:
+                max_state = -1
+            else:
+                max_state = 0
+        else:
+            if q[cur_state[0]][cur_state[1]][curplus1][cur_state[3]][cur_state[4]] > q[cur_state[0]][cur_state[1]][cur_state[2]][cur_state[3]][cur_state[4]]:
+                max_state = 1
+            else:
+                max_state = 0
 
     paddle2.y += (max_state*height/12.0)
 
@@ -178,17 +181,17 @@ def resetGame(game_state_tuple):
     game_state_tuple[4] = .5*height
 
 #Determine cur_state
-def determineState(cur_state, paddle, ball, velocity_x, velocity_y):
+def determineState(paddle, ball, velocity_x, velocity_y):
     n = 0
     while (width/1.0/columns)*(n+1) < (ball.left+ball.right)/2.0:
         n+=1
 
     m = 0
-    while (width/1.0/columns)*(m+1) < (ball.top+ball.bottom)/2.0:
+    while (height/1.0/rows)*(m+1) < (ball.top+ball.bottom)/2.0:
         m+=1
 
     l = 0
-    while (width/1.0/columns)*(m+1) < (paddle.top+paddle.bottom)/2.0:
+    while (height/1.0/rows)*(l+1) < (paddle.top+paddle.bottom)/2.0:
         l+=1
 
     if velocity_x < 0:
@@ -204,24 +207,65 @@ def determineState(cur_state, paddle, ball, velocity_x, velocity_y):
     else:
         j = 1
 
+    if m >= rows:
+        m = rows-1
+    if m < 0:
+        m = 0
+
+    if l >= rows:
+        l = rows-1
+    if l < 0:
+        l = 0
+
+    if n >= columns:
+        n = columns-1
+    if n < 0:
+        n = 0
+
     return (n,m,l,k,j)
 
 #Evaluate Q matrix
 def eval_q(q, r, prev_state, cur_state):
-    max_next_state = max(q[prev_state[0],prev_state[1],prev_state[2],-1,-1],
-                         q[prev_state[0],prev_state[1],prev_state[2],-1,0],
-                         q[prev_state[0],prev_state[1],prev_state[2],-1,1],
-                         q[prev_state[0],prev_state[1],prev_state[2],1,-1],
-                         q[prev_state[0],prev_state[1],prev_state[2],1,0],
-                         q[prev_state[0],prev_state[1],prev_state[2],1,1])
-    q[prev_state[0],prev_state[1],prev_state[2],prev_state[3],prev_state[4]] = r[prev_state[0],prev_state[0],prev_state[0],prev_state[0],prev_state[0]]+gamma*max_next_state
+    if prev_state[2] >= rows-1:
+        rowNumplus1 = rows-1
+    else:
+        rowNumplus1 = prev_state[2]+1
+    if prev_state[2] <= 0:
+        rowNumminus1 = 0
+    else:
+        rowNumminus1 = prev_state[2]-1
+    max_next_state = max(q[cur_state[0]][cur_state[1]][prev_state[2]][cur_state[3]][cur_state[4]],
+                         q[cur_state[0]][cur_state[1]][rowNumplus1][cur_state[3]][cur_state[4]],
+                         q[cur_state[0]][cur_state[1]][rowNumminus1][cur_state[3]][cur_state[4]])
+    ### This mess of a line determines the Q value for the last state as based on the lecture algorithm ###
+    q[prev_state[0]][prev_state[1]][prev_state[2]][prev_state[3]][prev_state[4]] = q[prev_state[0]][prev_state[1]][prev_state[2]][prev_state[3]][prev_state[4]]+(alpha/(learning_times[prev_state[0]][prev_state[1]][prev_state[2]][prev_state[3]][prev_state[4]]+alpha))*(r[prev_state[0]][prev_state[1]][prev_state[2]][prev_state[3]][prev_state[4]]+gamma*max_next_state-q[prev_state[0]][prev_state[1]][prev_state[2]][prev_state[3]][prev_state[4]])
 
+#Save the list in a file
+def save_q_learning(q):
+    ### r matrix [column][row_ball][row_paddle][x_speed][y_speed]###
+    with open(out_file, 'w') as outfile:
+        out_file.write(str(columns)+"\n")
+        out_file.write(str(rows)+"\n")
+        out_file.write(str(rows)+"\n")
+        out_file.write(str(2)+"\n")
+        out_file.write(str(3)+"\n")
+
+        for v in q:
+            for w in v:
+                for x in w:
+                    for y in x:
+                        for z in y:
+                            out_file.write(str(z)+"\n")
+    return
+#Load list from a file
+def load_q_learning(q):
+    return
 #Main function
 def main():
     pygame.init()
     global DISPLAYSURF
     ##Font information
-    global BASICFONT, BASICFONTSIZE
+    global BASICFONT, BASICFONTSIZE, learning_times
     ### [ball_x, ball_y, velocity_x, velocity_y, paddle_y] ###
     game_state_tuple = [.5*width, .5*height, .03, .01, .5*height]
 
@@ -271,7 +315,8 @@ def main():
 
     pygame.mouse.set_visible(False) # make cursor invisible
 
-    determineState(cur_state, paddle2, ball, game_state_tuple[2], game_state_tuple[3])
+    determineState(paddle2, ball, game_state_tuple[2], game_state_tuple[3])
+    gamesNum = 1
 
     while True: #main game loop
         for event in pygame.event.get():
@@ -283,6 +328,8 @@ def main():
             # play_again = raw_input("Press 'p' to play again")
             # if play_again != 'p':
             #     break
+            print("Game Number",gamesNum)
+            gamesNum += 1
             resetGame(game_state_tuple)
             paddle1 = pygame.Rect(PADDLEOFFSET, 0, paddle_width, paddle_height1)
             paddle2 = pygame.Rect(width - PADDLEOFFSET - paddle_width, game_state_tuple[4]-paddle_height2/2, paddle_width, paddle_height2)
@@ -302,14 +349,25 @@ def main():
             paddle1 = hard_coded_paddle (ball, paddle1)
             paddle2 = q_paddle(q, cur_state, paddle2)
 
-        displayScore(score)
+            displayScore(score)
 
-        pygame.display.update()
-        FPSCLOCK.tick(FPS)
+            pygame.display.update()
+            FPSCLOCK.tick(FPS)
 
-        prev_state = cur_state
-        determineState(cur_state, paddle2, ball, game_state_tuple[2], game_state_tuple[3])
-        eval_q(q, r, prev_state, cur_state)
+            prev_state = cur_state
+            cur_state = determineState(paddle2, ball, game_state_tuple[2], game_state_tuple[3])
+            print(cur_state)
+            learning_times[cur_state[0]][cur_state[1]][cur_state[2]][cur_state[3]][cur_state[4]]+=1
+            print(q[cur_state[0]][cur_state[1]][cur_state[2]])
+            print()
+            # for v in learning_times:
+            #     for w in v:
+            #         for x in w:
+            #             for y in x:
+            #                 for z in y:
+            #                     if z >0:
+            #                         print(z)
+            eval_q(q, r, prev_state, cur_state)
 
 if __name__=='__main__':
     # pdb.set_trace()
--- hw4/test.py
+++ hw4/test.py
@@ -1,5 +1,4 @@
-from random import randint
-
-myMatrix = [[[0 for x in range(10)] for y in range(5)] for z in range(3)]
-
-print(myMatrix[2][4][9])
+with open("./test.txt", 'w') as outfile:
+    outfile.write("x")
+    outfile.write("y")
+    outfile.write("z")
